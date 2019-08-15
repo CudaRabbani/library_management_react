9f1555980e04ce0d07d092c3a789d89007988760
CudaRabbani <md.reza.rabbani@gmail.com>
2019-08-14 23:51:11 -0600

    Book table filteration is implemented based on book category.

--- src/component/library/book/bookTable.jsx
+++ src/component/library/book/bookTable.jsx
@@ -4,9 +4,10 @@ import Noty from 'noty';
 
 import TableHeader from "../../common/table/tableHeader";
 import TableBody from "../../common/table/tableBody";
+import _ from "lodash";
 
 const api_endpoint = "http://localhost:4044/api/book";
-
+const category_api = 'http://localhost:4044/api/category';
 
 class BookTable extends Component {
     state={
@@ -35,11 +36,11 @@ class BookTable extends Component {
                 >Delete</button>
             }
         ],
-        books: []
+        books: [],
+        category: []
     };
 
     updateBook = (book) => {
-        console.log('Update: ', book);
         this.props.history.push('/books/edit/'+book._id);
     };
 
@@ -68,29 +69,69 @@ class BookTable extends Component {
         }
     };
 
+    handleCategorySideBar = (category) => {
+        console.log(category);
+        this.setState({selectedCategory: category});
+    };
+
+    getFilteredBooks() {
+        const {books: allBooks, selectedCategory} = this.state;
+        const filteredBooks = selectedCategory && selectedCategory._id ?
+            allBooks.filter(book => book.category._id === selectedCategory._id) : allBooks;
+        console.log('filtered books', filteredBooks);
+        return {totalFilteredBooks: filteredBooks.length,filteredBooks: filteredBooks };
+    }
+
 
     async componentDidMount() {
-        console.log('book table component');
         try {
             const {data} = await axios.get(api_endpoint);
             this.setState({books: data});
+            const {data: tempCategories} = await axios.get(category_api);
+            //console.log(data);
+            const categories = tempCategories.map(d=>_.pick(d,['_id','name']));
+            const category = [{'_id': '', 'name': 'All Categories'},...categories];
+            this.setState({category});
         }
-        catch (err) {
-            const {errors} = this.state;
-            errors.push(err.message);
-            this.setState({errors});
+        catch (ex) {
+            new Noty ({
+                theme: 'mint',
+                text: ex.response || ex.message,
+                type: "error",
+                timeout: 4000
+            }).show();
         }
     }
 
+
     render() {
+        const {selectedCategory} = this.state;
+        const {books} = this.state;
+        const {totalFilteredBooks, filteredBooks} = this.getFilteredBooks();
         return (
-            <table className="table m-1">
-                <TableHeader headerText={this.state.tableHeader}/>
-                <TableBody
-                    tableData={this.state.books}
-                    tableDataExtractor={this.state.tableDataExtractor}
-                />
-            </table>
+            <div className="row">
+                <div className="col-sm-3">
+                    <ul className="list-group">
+                        {this.state.category.map(cat=>(
+                            <li key={cat._id}
+                                className={selectedCategory === cat ? "list-group-item active" : "list-group-item"}
+                                onClick={()=>this.handleCategorySideBar(cat)}
+                            >{cat.name}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="col-sm-9">
+                    <p> Showing {totalFilteredBooks} Books out of {books.length} Books</p>
+                    <table className="table m-1">
+                        <TableHeader headerText={this.state.tableHeader}/>
+                        <TableBody
+                            /*tableData={this.state.books}*/
+                            tableData={filteredBooks}
+                            tableDataExtractor={this.state.tableDataExtractor}
+                        />
+                    </table>
+                </div>
+            </div>
         );
     }
 }
